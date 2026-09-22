5d826696f95c9d632db724113cd215493420b52c
abdulsamad73496-hue <abdulsamad73496@gmail.com>
2026-09-22 16:49:33 +0000

    Add files via upload

--- program1.c
+++ program1.c
@@ -1,11 +1,20 @@
+//Count  vowel and consonants  in a string ...
 #include<stdio.h>
-#include<string.h>
-int main()
+int main ()
 {
-char greeting[50]="welcome";
-    int len=strlen(greeting);
-    int size=sizeof(greeting);
-     printf("length%d", len);
-     printf("size%d", size);
-    return 0;
+int c=0,v=0,n;
+char A[n];
+printf("Enter how many charcater you have to print :");
+scanf("%d", &n);
+for (int i=0;i<n;i++)
+  {scanf("%c", &A[i]);}
+for (int k=0;k<n;k++)
+    { if ( A[k]=='a'||A[k]=='e'||A[k]=='i'||A[k]=='o'||A[k]=='u'||A[k]=='A'||A[k]=='E'||A[k]=='I'||A[k]=='O'||A[k]=='U')
+         {v=v+1;}
+      else 
+         {c=c+1;}
+     }
+printf("The no of  vowel %d\n",c);
+printf("The no of consonants %d",v);
+return 0;
 }
--- program2.c
+++ program2.c
@@ -1,1 +1,21 @@
-
+//lowercase string to uppercase ...
+#include<stdio.h>
+int main ()
+{
+int n;
+printf("How many char you have to input ");
+scanf("%d",&n);
+char a[n];
+for (int i=0;i<n;i++)
+    {scanf(" %c", &a[i]);}
+for (int k=0;k<n;k++)
+     {if  (a[k]>='a' && a[k]<='z')
+       {
+         a[k]=a[k]-32;
+         printf("%c", a[k]);
+       }
+     else 
+        {printf("%c",a[k]);}
+      }
+return 0;
+}
